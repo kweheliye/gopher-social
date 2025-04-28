8bbb67b80d3425d37178acee205c1cb48f2d98cb
Kamal Hashi <kamalweheliye@gmail.com>
2025-04-28 15:52:19 +0400

    add migration for user and post

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"github.com/kweheliye/gopher-social/internal/db"
 	"github.com/kweheliye/gopher-social/internal/env"
+	store2 "github.com/kweheliye/gopher-social/internal/store"
 	"log"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", ""),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+	// Main Database
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("Connected to database")
+
+	store := store2.NewStorage(nil)
 
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/kweheliye/gopher-social
 
 go 1.24.2
 
-require github.com/go-chi/chi/v5 v5.2.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+)
